f7175c4b550d5fc5e39f27bdbcb09d64f44da67b
Vakor PC <vakor.dev@gmail.com>
2023-03-06 21:37:33 +0200

    IntersectableList

---
+++ Sources/RayTracer.Library/Shapes/IntersectableList.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using RayTracer.Library.Mathematics;
+
+namespace RayTracer.Library.Shapes;
+
+public class IntersectableList : List<IIntersectable>, IIntersectable
+{
+    private IIntersectable? _lastIntersectable;
+    public IntersectableList(IIntersectable[] intersectables) : base(intersectables)
+    {
+    }
+    public bool TryIntersect(in Ray ray, out IntersectionResult result)
+    {
+        result = default;
+        bool resultExists = false;
+        float t = float.MaxValue;
+
+        foreach (IIntersectable intersectable in this)
+        {
+            if (intersectable.TryIntersect(ray, out IntersectionResult temp))
+            {
+                if (temp.T < t)
+                {
+                    result = temp;
+                    t = temp.T;
+                    resultExists = true;
+                }
+            }
+        }
+
+        return resultExists;
+    }
+
+    public Vector3 GetNormal(in Vector3 point)
+    {
+        Ray ray = new(point, Vector3.Zero);
+        foreach (var intersectable in this)
+        {
+            if(intersectable.TryIntersect(ray, out _))
+            {
+                return intersectable.GetNormal(point);
+            }
+        }
+        
+        throw new System.Exception("No intersectable found");
+    }
+}
