0b12864ede7484ed5cd80a9fa250234f50d2a24a
Vakor PC <vakor.dev@gmail.com>
2023-03-08 00:45:41 +0200

    + DiscSerializer

---
+++ Sources/RayTracer.Library/Serialization/Serializers/DiscSerializer.cs
@@ -0,0 +1,54 @@
+using System.Text.Json;
+using RayTracer.Library.Extensions;
+using RayTracer.Library.Mathematics;
+using RayTracer.Library.Shapes;
+
+namespace RayTracer.Library.Serialization.Serializers;
+
+public class DiscSerializer : SerializerBase<DiscSerializer, Disc>
+{
+    public override void Serialize(Utf8JsonWriter writer, Disc? value)
+    {
+        if (value is null)
+        {
+            writer.WriteNullValue();
+            return;
+        }
+        
+        writer.WriteStartObject();
+
+        writer.WritePropertyName(nameof(value.Radius));
+        writer.WriteNumberValue(value.Radius);
+
+        writer.WritePropertyName(nameof(value.Center));
+        Vector3Serializer.Instance.Serialize(writer, value.Center);
+        
+        writer.WritePropertyName(nameof(value.Normal));
+        Vector3Serializer.Instance.Serialize(writer, value.Normal);
+
+        writer.WriteEndObject();
+    }
+
+    public override Disc? Deserialize(ref Utf8JsonReader reader)
+    {
+        if (reader.TryReadNull())
+            return null;
+
+        reader.EnsureTokenAndRead(JsonTokenType.StartObject);
+
+        reader.EnsurePropertyAndRead("Radius"u8);
+        reader.EnsureToken(JsonTokenType.Number);
+        float radius = reader.GetSingle();
+        reader.Read();
+
+        reader.EnsurePropertyAndRead("Center"u8);
+        Vector3 center = Vector3Serializer.Instance.Deserialize(ref reader);
+        reader.Read();
+        
+        reader.EnsurePropertyAndRead("Normal"u8);
+        Vector3 normal = Vector3Serializer.Instance.Deserialize(ref reader);
+        reader.Read();
+        
+        return new(center, normal, radius);
+    }
+}
