368fb436da5a62c45a6440543cc1f0b867a0abdb
prokuror <a.sharp.fus@gmail.com>
2023-03-08 11:52:19 +0200

    * fixed time comparison

--- Sources/RayTracer.Library.Tests/Shapes/IIntersactableTests.cs
+++ Sources/RayTracer.Library.Tests/Shapes/IIntersactableTests.cs
@@ -6,7 +6,7 @@ namespace RayTracer.Library.Tests.Shapes;
 public static class IntersectionResultTestHelper
 {
     public static bool IsCorrect(this IntersectionResult intersectionResult) =>
-        intersectionResult.T > 0;
+        intersectionResult.T >= 0;
     
         // && Math.Abs(intersectionResult.Normal.Length() - 1) < 1e-6
 }
