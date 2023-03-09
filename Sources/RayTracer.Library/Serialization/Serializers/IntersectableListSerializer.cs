0a44cb05571ed3e1de2152c8befa0c59acf10f08
Vakor PC <vakor.dev@gmail.com>
2023-03-09 10:51:12 +0200

    + IntersectableListSerializer

---
+++ Sources/RayTracer.Library/Serialization/Serializers/IntersectableListSerializer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Text.Json;
+using RayTracer.Library.Extensions;
+using RayTracer.Library.Shapes;
+
+namespace RayTracer.Library.Serialization.Serializers;
+
+public class IntersectableListSerializer : SerializerBase<IntersectableListSerializer, IntersectableList>
+{
+    private readonly ISerializer<IIntersectable> _itemSerializer;
+
+    public IntersectableListSerializer()
+    {
+        _itemSerializer = new PolymorphicSerializer<IIntersectable>();
+    }
+
+    public override void Serialize(Utf8JsonWriter writer, IntersectableList? value)
+    {
+        if (value == null) throw new ArgumentNullException(nameof(value));
+
+        writer.WriteStartArray();
+
+        foreach (var item in value)
+        {
+            _itemSerializer.Serialize(writer, item);
+        }
+
+        writer.WriteEndArray();
+    }
+
+    public override IntersectableList Deserialize(ref Utf8JsonReader reader)
+    {
+        IntersectableList intersectableList = new IntersectableList();
+
+        reader.EnsureTokenAndRead(JsonTokenType.StartArray);
+
+        while (reader.TokenType != JsonTokenType.EndArray)
+        {
+            IIntersectable item = _itemSerializer.Deserialize(ref reader)!;
+            intersectableList.Add(item);
+        }
+        
+        reader.EnsureTokenAndRead(JsonTokenType.EndArray);
+
+        return intersectableList;
+    }
+}
